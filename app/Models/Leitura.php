dad2d0c578260f584dfee624d7ae841f15066257
camillofranco <camillo.samcro@gmail.com>
2025-11-04 14:41:23 -0300

    feat(backend): adiciona models, migrations, seeders, policies e controllers base

---
+++ app/Models/Leitura.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Leitura extends Model
+{
+    /** @use HasFactory<\Database\Factories\LeituraFactory> */
+    use HasFactory;
+}
---
+++ database/seeders/LeituraSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class LeituraSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
